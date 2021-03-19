faf2336066a6f6c0c7971469ee2ff952f9da0cd8
Barri <info@bdesignstudio.co.uk>
2021-03-19 18:58:00 +0000

    add win

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+
+let score = 0;
 function getSad() {
-  return Date.now() + 1000
+  return Date.now() + 1000;
 }
 
 function getGone() {
   return Date.now() + Math.floor(Math.random() * 18000) + 2000;
 }
+function getHungry() {
+  return Date.now() + Math.floor(Math.random() * 3000) + 2000;
+}
+
 const moles = [
   {
     status: "sad",
@@ -71,30 +77,75 @@ const moles = [
 function getNextStatus(mole) {
   switch (mole.status) {
     case 'sad':
+    case 'fed':
       mole.next = getSad();
       mole.status = 'leaving';
-      mole.node.src = './img/mole-leaving.png';
+      mole.node.children[0].src = './mole-leaving.png';
       break;
+
     case 'leaving':
-      mole.next = getGone() ;
+      mole.next = getGone();
       mole.status = 'gone';
       mole.node.children[0].classList.add('gone')
       break;
+
+    case 'gone':
+      mole.status = 'hungry';
+      mole.next = getHungry();
+      mole.node.children[0].classList.add('hungry');
+      mole.node.children[0].classList.remove('gone');
+      mole.node.children[0].src = './mole-hungry.png';
+      break;
+
+    case 'hungry':
+      mole.status = 'sad';
+      mole.next = getSad();
+      mole.node.children[0].classList.remove('hungry');
+      mole.node.children[0].src = './mole-sad.png';
+      break;
+
   }
+}
+function feed(event) {
+  if (event.target.tagName !== 'IMG' || !event.target.classList.contains('hungry')) {
+    return;
+  }
+  console.log(parseInt(event.target.dataset.index))
+
+  const mole = moles[parseInt(event.target.dataset.index)]
+  mole.status = 'fed';
+  mole.next = getSad();
+  mole.node.children[0].src = './mole-fed.png'
+  mole.node.children[0].classList.remove('hungry');
+  score++;
+  if (score >= 3) {
+    win();
+  }
+
+}
+
+function win(){
+  document.querySelector('.bg').classList.add('hide');
+  document.querySelector('.win').classList.remove('hide');
+
 }
 let runAgainAt = Date.now() + 100;
 
 function nextFrame() {
-  const now = Date.now()
-  if (runAgainAt < now) {
+  const now = Date.now();
 
-    for (i of moles) {
-      if (moles[i].next < now) {
-        getNextStatus(mole[i]);
+  if (runAgainAt <= now) {
+    for (let i = 0; i < moles.length; i++) {
+      if (moles[i].next <= now) {
+        getNextStatus(moles[i]);
       }
     }
     runAgainAt = now + 100;
     requestAnimationFrame(nextFrame)
   }
 }
+
+
+document.querySelector('.bg').addEventListener('click', feed)
+
 nextFrame();
